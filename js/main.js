27b46699a4f3604d15fe715d41ce66753992cfcf
Abdelrahman Eid <eida0556@gmail.com>
2025-07-20 20:50:22 +0300

    first version

---
+++ js/main.js
@@ -0,0 +1,213 @@
+let showInventoryBtn = document.querySelector(".showInventory")
+let curdProduct = document.querySelector(".curd-product")
+let all_inputs = document.querySelectorAll(".row input")
+let allCalcInputs = document.querySelectorAll(".calc input")
+let name_product =document.getElementById("name_product")
+let category_product =document.getElementById("category_product")
+let number_product =document.getElementById("number_product")
+let image_product =document.getElementById("image_product")
+let net_cost_product =document.getElementById("net_cost")
+let discount_product =document.getElementById("discount_product")
+let cost_product =document.getElementById("cost_product")
+let net_sales_product =document.getElementById("net_sales")
+let tax_product =document.getElementById("tax")
+let price_product =document.getElementById("price")
+let net_profit_product =document.getElementById("net_profit")
+let btn_Add_product = document.getElementById("add_product")
+let btn_clear_product = document.getElementById("clear_product")
+let search = document.getElementById("search")
+let alert_message = document.querySelector(".alert_message")
+
+let table = document.getElementById('table')
+let tbody = document.getElementById('tbody')
+
+//====================== local sotrage =====================================
+let editIndex = null
+let products;
+if(localStorage.data != null){
+    products = JSON.parse(localStorage.data)
+    showData()
+}
+else{
+    products = []
+}
+//====================== check =======================================
+function check(){
+    if(tbody.childElementCount == 0 && localStorage.data == null || products ==""){
+        alert_message.classList.remove("none")
+        table.classList.add("none")
+    }
+    else{
+        alert_message.classList.add("none")
+        table.classList.remove("none")
+    }
+}
+check()
+//====================== checkInputs =======================================
+function checkInputs() {
+    let hasText = false;
+    
+    allCalcInputs.forEach((i) => {
+        let oldMsg = i.nextElementSibling;
+        if (oldMsg && oldMsg.classList.contains("text-danger")) {
+        oldMsg.remove();
+        }
+        if (isNaN(i.value)) {
+            let p = document.createElement("p")
+            p.textContent = "only Number's"
+            p.classList.add("text-danger")
+            i.after(p)
+            hasText = true;
+            i.classList.add("border-danger");
+        } else {
+            i.classList.remove("border-danger");
+        }
+    });
+    return hasText;
+}
+//===================== createData ===========================================
+function createData(){
+    btn_Add_product.innerHTML = `اضافة المنتج`
+    let createProducts = {
+        name : name_product.value,
+        category : category_product.value,
+        image : image_product.value,
+        number : number_product.value,
+        costProduct : cost_product.value,
+        discount : discount_product.value,
+        netCostProduct : net_cost_product.value,
+        price : price_product.value,
+        tax : tax_product.value,
+        netSales : net_sales_product.value,
+        netProfitProduct : net_profit_product.value,
+    }
+    if(editIndex !== null){
+    products[editIndex] = createProducts;
+    editIndex = null;
+    btn_Add_product.innerHTML = `اضافة المنتج`;
+    }else {
+    products.push(createProducts);
+    }
+    localStorage.setItem("data",JSON.stringify(products))
+    curdProduct.style.display = "none"
+    showInventory()
+    showData()
+    clear()
+}
+//====================== calc =================================================
+function calcNetProfit(){
+    // Net Cost
+    if(checkInputs())return;
+    function netCost(){
+        let costProduct = cost_product.value;
+        let discount = discount_product.value;
+        let netCost = costProduct - (costProduct * (discount / 100))
+        net_cost_product.value = netCost * number_product.value
+    }
+    // Net Price 
+    function netPrice(){
+        let price = +price_product.value;
+        let tax = +tax_product.value;
+        let totalPrice = price + (price * (tax / 100));
+        net_sales_product.value = totalPrice * +number_product.value;
+    }
+    // Net Profit
+    function netProfit(){
+        net_profit_product.value = +net_sales_product.value -  +net_cost_product.value;
+        if(+net_cost_product.value > +net_sales_product.value){
+            net_profit_product.classList.add("bg-danger")
+        }else{
+            net_profit_product.classList.remove("bg-danger")
+            net_profit_product.classList.add("profit")
+        }
+    }
+    netCost()
+    netPrice()
+    netProfit()
+}
+allCalcInputs.forEach((input) => {
+    input.addEventListener("keyup", calcNetProfit);
+});
+
+//====================== showData =============================================
+function showData(product = products){
+    tbody.innerHTML =""
+    product.forEach((i,index)=>{
+        tbody.innerHTML +=`
+        <tr>
+        <td>${index + 1}</td>
+        <td>${i.name}</td>
+        <td>${i.category}</td>
+        <td><img src="${i.image}" style="max-width:55px"></td>
+        <td>${i.netSales}</td>
+        <td>${i.number}</td>
+        <td><span><i class="fa-solid fa-pen-to-square text-info edit" data-index="${index}"></i></span></td>
+        <td><span><i class="fa-solid fa-trash-can text-danger delete" data-index="${index}"></i></span></td>
+        </tr>
+        `
+        check()
+    })
+}
+//==================== showInventory ==============================
+let inventory = "show"
+function showInventory(){
+    if(inventory == "hidden" || curdProduct.style.display != "block"){
+        showInventoryBtn.innerHTML = `اخفاء`
+        curdProduct.style.display = "block"
+        inventory = "show"
+    }
+    else{
+        showInventoryBtn.innerHTML = `اضافة`
+        curdProduct.style.display = "none"
+        inventory = "hidden"
+    }
+}
+showInventory()
+showInventoryBtn.addEventListener("click",showInventory)
+//==================== search =====================================
+search.addEventListener("keyup",()=>{
+    let searchProduct = products.filter((i)=>
+        i.name.includes(search.value)
+    )
+    showData(searchProduct)
+})
+//==================== clear ========================================
+function clear(){
+    all_inputs.forEach((i)=>{
+        i.value =""
+    })
+    let defaultValue = "اختر من القائمة"
+    category_product.value = defaultValue
+    number_product.value = 1
+}
+btn_clear_product.addEventListener("click",clear)
+//==================== add ==========================================
+btn_Add_product.addEventListener("click",createData)
+//==================== delete =======================================
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("delete")){
+        let row = e.target.dataset.index
+        products.splice(row,1);
+        localStorage.setItem("data",JSON.stringify(products))
+        showData()
+        check()
+    }
+})
+//=================== edit ===========================================
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("edit")){
+        let row = e.target.dataset.index
+        name_product.value = products[row].name;
+        category_product.value = products[row].category;
+        image_product.value = products[row].image;
+        net_sales_product.value = products[row].netSales;
+        number_product.value = products[row].number;
+        cost_product.value = products[row].costProduct;
+        net_cost_product.value = products[row].netCostProduct;
+        discount_product.value = products[row].discount;
+        price_product.value = products[row].price;
+        net_profit_product.value = products[row].netProfitProduct;
+        btn_Add_product.innerHTML = `حفظ التعديلات`
+        editIndex = row;       
+    }
+})
